f4b61fcd149089dc6481eb985957acc30f67a4ac
jiecchen <jiecchen@indiana.edu>
2015-10-29 15:41:02 -0400

    added

---
+++ include/streamcc_test.h
@@ -0,0 +1,17 @@
+// include all headers
+// for test only
+
+#ifndef __STREAMCC_TEST__
+#define __STREAMCC_TEST__
+
+#include "streamcc_headers/Sampling.hpp"
+#include "streamcc_headers/CountMin.hpp"
+#include "streamcc_headers/CountMin_basic.h"
+#include "streamcc_headers/CountSketch.hpp"
+#include "streamcc_headers/CountSketch_basic.h"
+#include "streamcc_headers/Hash.h"
+//#include "streamcc_headers/Algebra.hpp"
+
+
+
+#endif
