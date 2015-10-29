7c2af3113df421332638348c5b50ac6559376d1d
jiecchen <jiecchen@indiana.edu>
2015-10-29 17:54:18 -0400

    added

---
+++ src/utils.cpp
@@ -0,0 +1,94 @@
+#include "../include/streamcc_headers/utils.hpp"
+#include <random>
+#include <cmath>
+#include <stdint.h>
+
+
+namespace utils {
+  const int MAX = 1 << 30;
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_real_distribution<> dis(0, 1);
+  std::default_random_engine generator;
+  std::uniform_int_distribution<> dis_int(0, MAX);  
+}
+
+
+
+int utils::rand_int(int M) {
+  return dis_int(gen) % M;
+}
+
+
+double utils::find_closest(double a, double b) {
+  int j = std::ceil(std::log(b) / std::log(a));
+  return std::pow(a, j);      
+}
+
+
+double utils::rand_double() {
+    return dis(gen);
+}
+
+
+double utils::Gaussian::rand() {
+  return dist(gen);
+}
+
+uint32_t utils::zeros(uint32_t x) {
+  static uint8_t const clz_lkup[] = {
+    32U, 31U, 30U, 30U, 29U, 29U, 29U, 29U,
+    28U, 28U, 28U, 28U, 28U, 28U, 28U, 28U
+  };
+  uint32_t n;
+
+  if (x >= (1U << 16)) {
+    if (x >= (1U << 24)) {
+      if (x >= (1 << 28)) {
+	n = 28U;
+      }
+      else {
+	n = 24U;
+      }
+    }
+    else {
+      if (x >= (1U << 20)) {
+	n = 20U;
+      }
+      else {
+	n = 16U;
+      }
+    }
+  }
+  else {
+    if (x >= (1U << 8)) {
+      if (x >= (1U << 12)) {
+	n = 12U;
+      }
+      else {
+	n = 8U;
+      }
+    }
+    else {
+      if (x >= (1U << 4)) {
+	n = 4U;
+      }
+      else {
+	n = 0U;
+      }
+    }
+  }
+  return (uint32_t)clz_lkup[x >> n] - n;
+}
+
+
+
+
+
+
+
+
+
+
+
+
